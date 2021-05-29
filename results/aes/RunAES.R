e580e25119e29b5922fe730acb682f855884d47e
Ankit <ankitk@ncbs.res.in>
2021-05-29 22:46:44 +0200

    Added Multiplication benchmarks

--- results/aes/RunAES.R
+++
@@ -1,1 +0,0 @@
-filename pn pc hcount maxcost p_rhorn p_incrhorn timing
--- src/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$1" || { printf 'unable to navigate to target\n' >&2; exit 1 ; }
-for file in *.out; do
-    test -f "$file" || continue
-    printf "$file "
-    awk '{print "    "$NF}' "$file" | awk '{ sum += $1 } END { print sum }'
-
-done
